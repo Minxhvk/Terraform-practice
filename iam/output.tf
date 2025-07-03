c4eeec71d75ea16690b8dd85dec788f797778f2a
Minxhvk <minxhvk@gmail.com>
2025-07-03 13:51:50 +0900

    variables

---
+++ iam/output.tf
@@ -0,0 +1,16 @@
+output "tf_image_id" {
+    value = var.image_id
+}
+
+output "tf_availabilitu_zone_names" {
+    value = var.availability_zone_names
+}
+
+output "tf_ami_id_maps" {
+  value = var.ami_id_maps
+}
+
+output "tf_first_availability_zone_names" {
+    value = var.availability_zone_names[0]
+}
+
