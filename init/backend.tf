a5c6c856db0064be3b36e2de1c68a37c82073685
Minxhvk <minxhvk@gmail.com>
2025-07-03 14:05:13 +0900

    provisioning

--- init/backend.tf
+++ init/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" { # 강의는 
-      bucket         = "tf-jupiter-apne2-tfstate" # s3 bucket 이름
-      key            = "terraform/terraform.tfstate" # s3 내에서 저장되는 경로를 의미합니다.
+    backend "s3" {
+      bucket         = "tf-jupiter-apne2-tfstate"
+      key            = "terraform/terraform.tfstate" # s3 내에서 저장되는 경로
       region         = "ap-northeast-2"  
       encrypt        = true
       dynamodb_table = "terraform-lock"
